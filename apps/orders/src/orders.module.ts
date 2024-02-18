7eb44eb219d689fcbfd2a2cc82576354acb4fc35
Psyon <sayan.h2020@gmail.com>
2024-02-18 20:06:15 +0330

    v1.0.1

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
+import { DatabaseModule, RmqModule, AuthModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
-import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './orders.repository';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { BILLING_SERVICE } from './constants/services';
 
 @Module({
   imports: [
@@ -11,12 +15,18 @@ import { DatabaseModule } from '@app/common';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
       }),
-      envFilePath: './app/orders/.env',
+      envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    RmqModule.register({
+      name: BILLING_SERVICE,
+    }),
+    AuthModule,
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
